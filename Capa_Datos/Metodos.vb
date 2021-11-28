3882c8fcd4aa1ba0c96156af1a4e470f9f86428b
alexwu36 <alex.wu@utp.ac.pa>
2021-11-28 00:51:53 -0500

    añadiendo equipos dgv

---
+++ Capa_Datos/Metodos.vb
@@ -0,0 +1,36 @@
+﻿Imports System.Data.SqlClient
+Public Class Metodos
+    Public Shared Function BUSCAREQUIPO()
+        Using CN As New SqlConnection(My.Settings.Conexion)
+            Using CMD As New SqlCommand("select nombreEquipo as Equipo, cantDispo as Cantidad from automatizacion.equipo;", CN)
+                CMD.CommandType = CommandType.Text
+                Using DA As New SqlDataAdapter(CMD)
+                    Using TABLA As New DataTable
+                        DA.Fill(TABLA)
+
+                        Return TABLA
+                    End Using
+                End Using
+            End Using
+        End Using
+
+    End Function
+
+    Public Shared Function TipoUsuario(usuario As String) As Integer
+        Using CN As New SqlConnection(My.Settings.Conexion)
+            CN.Open()
+            Using CMD As New SqlCommand("SELECT TipoUsuario FROM automatizacion.usuario WHERE email= '" & usuario & "'", CN)
+                CMD.CommandType = CommandType.Text
+                Dim valor As Int32 = Convert.ToInt32(CMD.ExecuteScalar())
+                CN.Close()
+
+                Return valor
+            End Using
+        End Using
+
+    End Function
+
+
+
+End Class
+
--- Capa_Presentacion/Form_SEquipo2.vb
+++ Capa_Presentacion/Form_SEquipo2.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form_SEquipo2
     Private Sub Form_SEquipo2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
+        dgvInfoEquipo.DataSource = Capa_Datos.Metodos.BUSCAREQUIPO()
     End Sub
 
     Private Sub btn_Regresar_SE2_Click(sender As Object, e As EventArgs) Handles btn_Regresar_SE2.Click
@@ -14,6 +14,6 @@
     End Sub
 
     Private Sub dgvInfoEquipo_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgvInfoEquipo.CellContentClick
-
+        dgvInfoEquipo.DataSource = Capa_Datos.Metodos.BUSCAREQUIPO()
     End Sub
 End Class
